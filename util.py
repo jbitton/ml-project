2d6b311b35bbdc06ac7dd3544386bd7eb551832f
Joanna Bitton <joanna.bitton@gmail.com>
2018-05-01 20:19:35 -0400

    add roc curve

--- util.py
+++ util.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+
 def results(y_pred, y_test):
     """
     :param y_pred: the predicted y-values
@@ -12,3 +16,19 @@ def results(y_pred, y_test):
         else:
             num_wrong += 1
     return num_right, num_wrong, (num_right/(num_right + num_wrong))
+
+
+def roc_results(y_pred, y_test):
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred, pos_label=1)
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr,
+             lw=lw, label=f'ROC curve (AUC = {round(auc(fpr, tpr), 2)})')
+    plt.plot([0, 1], [0, 1], lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
