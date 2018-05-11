ebd5bb630924680de9c7ba1538765ecc1a72f5df
Joanna Bitton <joanna.bitton@gmail.com>
2018-05-11 18:07:31 -0400

    add feature importance for lr

--- conditional_probability/lr.py
+++ conditional_probability/lr.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 from util import roc_results, results
@@ -41,3 +43,27 @@ class LR(BaseModel):
         y_pred = self.predict_probability()
         roc_results(y_pred[:, 1], self.y_test, 'Logistic Regression')
         return roc_auc_score(self.y_test, y_pred[:, 1]), results(y_pred_class, self.y_test)
+
+    def plot_feature_importance(self):
+        """
+        This function plots the feature importances in the random forest classifier
+
+        ***NOTE*** THIS FUNCTION WILL ONLY RUN IF THERE ARE NO INTERACTION FEATURES
+        """
+        columns = np.array(['G1', 'G2', 'G3', 'G4', 'G7', 'G8', 'G10', 'G11', 'G12',
+                            'G13', 'G15', 'G17', 'G22', 'G23', 'G24', 'G25', 'G26',
+                            'G27', 'G28', 'G31', 'G34', 'G35', 'G36', 'G37', 'G39',
+                            'G40', 'G42', 'G43', 'G44', 'G45', 'HCT', 'PLT', 'WBC',
+                            'HGL', 'Age', 'TG', 'Bias'])
+        importances = self.model.coef_[0]
+        indices = np.argsort(importances)[::-1]
+        indices = [i for i in indices if importances[i] > 0]
+
+        plt.figure()
+        plt.title("Feature Importances: Logistic Regression")
+        plt.bar(range(len(indices)), importances[indices], color="r", align="center")
+        plt.xticks(range(len(indices)), columns[indices], rotation=90)
+        plt.xlim([-1, len(indices)])
+        plt.xlabel('Feature')
+        plt.ylabel('Feature Importance')
+        plt.show()
